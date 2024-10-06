2c60b6bcf8dc0dde16d9df219633863fe00c61e3
SergKudr72 <savat28@yandex.ru>
2024-10-06 15:39:13 +0500

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,46 @@
+class WordsFinder:
+
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                line = file.read().lower()
+                for punctuation in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    line = line.replace(punctuation, '')
+                words = line.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        plase = {}
+        for name, words in self.get_all_words().items():
+            if word.lower() in words:
+                plase[name] = words.index(word.lower()) + 1
+        return plase
+
+    def count(self, word):
+        counter = {}
+        for name, words in self.get_all_words().items():
+            counter[name] = words.count(word.lower())
+            return counter
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+
+# Поисковое слово: 'if'
+
+# Код:
+
+# finder1 = WordsFinder('Rudyard Kipling - If.txt',)
+#
+# print(finder1.get_all_words())
+# print(finder1.find('if'))
+# print(finder1.count('if'))
+
+
